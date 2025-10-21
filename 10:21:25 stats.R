c0253aef20e4db3246d68c9cbeb62d7facb45616
irocsupreme2016 <irocsupreme2016@gmail.com>
2025-10-21 16:06:30 -0400

    Add files via upload

---
+++ 10:21:25 stats.R
@@ -0,0 +1,14 @@
+#Authur:Pawar;Date: 10/21/2025;Purpose: Calculate T-test
+
+#Create some dummy dataset for 2 variables 
+x=rnorm(10)
+y=rnorm(10)
+
+#Create a density plot 
+pts=seq(-4,5,5)
+plot(pts)dt(pts,df=9),col='red',type='1'
+lines(density(x),col='green')
+lines(density(y),col='blue'
+
+Calculate T test
+ttest=t.test(x,y)
